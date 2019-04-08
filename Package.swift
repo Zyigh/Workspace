868c21e2ab318415b284423b64d72fb0c67400c6
Hugo Medina <hmedina.paris75@gmail.com>
2019-04-08 10:30:39 +0200

    with correct syntaxe...

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "workspace",
     dependencies: [
         .package(url: "https://github.com/oarrabi/Guaka.git", from: "0.3.1"),
-        .Package(url: "https://github.com/stencilproject/Stencil.git", majorVersion: 0, minor: 13),
+        .package(url: "https://github.com/stencilproject/Stencil.git", from: "0.13.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
