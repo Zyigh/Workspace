27257955225bb22859c7a63c61754156ac4070fb
Hugo Medina <hmedina.paris75@gmail.com>
2019-04-08 10:29:50 +0200

    Stencil used as template engine

--- Package.swift
+++ Package.swift
@@ -7,13 +7,14 @@ let package = Package(
     name: "workspace",
     dependencies: [
         .package(url: "https://github.com/oarrabi/Guaka.git", from: "0.3.1"),
+        .Package(url: "https://github.com/stencilproject/Stencil.git", majorVersion: 0, minor: 13),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "workspace",
-            dependencies: ["Guaka"]),
+            dependencies: ["Guaka", "Stencil"]),
         .testTarget(
             name: "workspaceTests",
             dependencies: ["workspace"]),
